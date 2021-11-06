18aed8e1112d70a93f69b7b5ad8732d42b9a8eec
gpaetzold <76201189+gpaetzold@users.noreply.github.com>
2021-11-06 14:37:01 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,6 @@
 let sensor = 0
 basic.showIcon(IconNames.Happy)
-let tm = TM1637.create(
-DigitalPin.P2,
-DigitalPin.P3,
-7,
-4
-)
 basic.forever(function () {
     sensor = pins.analogReadPin(AnalogPin.P1)
     basic.showNumber(sensor)
-    tm.on()
-    tm.showNumber(sensor)
 })
